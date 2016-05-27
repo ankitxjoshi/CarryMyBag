7b0689a00955cf0c8bf5976865f3330270214381
MacBox_7 <ajankit2304@gmail.com>
2016-05-27 12:32:12 +0530

    Initial commit

---
+++ app/src/main/java/com/carrymybag/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.carrymybag.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://192.168.1.3/carrymybag/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://192.168.1.3/carrymybag/register.php";
+}
