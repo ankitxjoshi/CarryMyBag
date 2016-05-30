992eaa1db7934d4e92613cdb2a4a011e0129fcbd
MacBox_7 <ajankit2304@gmail.com>
2016-05-30 19:17:03 +0530

    With Material Design

--- app/src/main/java/com/carrymybag/activity/FirstFragment.java
+++ app/src/main/java/com/carrymybag/activity/FirstFragment.java
@@ -15,25 +15,29 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
 public class FirstFragment extends android.support.v4.app.Fragment {
 
-    public TextView textViewName;
-    public TextView textViewEmail;
-
-    //TextViews
-    private NetworkImageView profilePhoto;
-
-    //Image Loader
-    private ImageLoader imageLoader;
+    public AutoCompleteTextView fromCityAuto;
+    public AutoCompleteTextView fromCityTo;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.activity_first_fragment,null);
-
+        View v = inflater.inflate(R.layout.activity_first_fragment,null);
+        fromCityAuto = (AutoCompleteTextView)v.findViewById(R.id.fromcityauto);
+        fromCityTo = (AutoCompleteTextView)v.findViewById(R.id.fromcityauto);
+        String[] cityfrom = {"Surat","Ahmedabad"};
+        String[] cityto = {"Surat","Ahmedabad"};
+        ArrayAdapter<String> tocityadapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_expandable_list_item_1,cityto);
+        ArrayAdapter<String> fromcityadapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_expandable_list_item_1,cityfrom);
+        fromCityAuto.setAdapter(fromcityadapter);
+        fromCityTo.setAdapter(tocityadapter);
+        return v;
 
     }
 }
