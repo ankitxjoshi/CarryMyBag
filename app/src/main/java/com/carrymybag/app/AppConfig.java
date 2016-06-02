481b8b2464cfced4e570d899995acf35cfa1ade3
Anshu Kumar <kumaranshu72>
2016-06-02 22:41:44 +0530

    lo ji ho gya text color change

--- app/src/main/java/com/carrymybag/app/AppConfig.java
+++ app/src/main/java/com/carrymybag/app/AppConfig.java
@@ -2,11 +2,11 @@ package com.carrymybag.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://192.168.1.5/carrymybag/login.php";
+	public static String URL_LOGIN = "http://192.168.1.102/carrymybag/login.php";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://192.168.1.5/carrymybag/register.php";
+	public static String URL_REGISTER = "http://192.168.1.102/carrymybag/register.php";
 
 	// Server user register url
-	public static String URL_CityList = "http://192.168.1.5/carrymybag/retreivecity.php";
+	public static String URL_CityList = "http://192.168.1.102/carrymybag/retreivecity.php";
 }
