f14c240a455c2e55042aa89615b78422c1a199b6
Anshu Kumar <kumaranshu72>
2016-06-12 07:37:48 +0530

    svs

--- app/src/main/java/com/carrymybag/app/AppConfig.java
+++ app/src/main/java/com/carrymybag/app/AppConfig.java
@@ -2,13 +2,13 @@ package com.carrymybag.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://192.168.1.8/carrymybag/login.php";
+	public static String URL_LOGIN = "http://192.168.1.100/carrymybag/login.php";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://192.168.1.8/carrymybag/register.php";
+	public static String URL_REGISTER = "http://192.168.1.100/carrymybag/register.php";
 
 	// Server user retrieve city url
-	public static String URL_CityList = "http://192.168.1.8/carrymybag/retreivecity.php";
+	public static String URL_CityList = "http://192.168.1.100/carrymybag/retreivecity.php";
 
-	public static String URL_Storedata = "http://192.168.1.8/carrymybag/storedata.php";
+	public static String URL_Storedata = "http://192.168.1.100/carrymybag/storedata.php";
 }
