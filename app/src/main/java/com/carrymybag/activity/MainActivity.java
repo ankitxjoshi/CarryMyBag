50fc33ee047c524ba5c9d6c7cd9a2e70731985e8
MacBox_7 <ajankit2304@gmail.com>
2016-06-18 22:46:26 +0530

    Navigation Updated

--- app/src/main/java/com/carrymybag/activity/MainActivity.java
+++ app/src/main/java/com/carrymybag/activity/MainActivity.java
@@ -1,110 +1,189 @@
 package com.carrymybag.activity;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
 import com.carrymybag.R;
-import com.carrymybag.adapter.RecyclerViewAdapter;
-import com.carrymybag.helper.SQLiteHandler;
-import com.carrymybag.helper.SessionManager;
-
-import java.util.HashMap;
-
-
-import android.content.res.TypedArray;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-    public DrawerLayout drawerLayout;
-    RecyclerView recyclerView;
-    String navTitles[];
-    TypedArray navIcons;
-    RecyclerView.Adapter recyclerViewAdapter;
-    ActionBarDrawerToggle drawerToggle;
+    //Defining Variables
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    public static NetworkImageView profilePhoto;
+
+    //Image Loader
+    public ImageLoader imageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Let's first set up toolbar
-        setupToolbar();
-
-        //Initialize Views
-        recyclerView  = (RecyclerView) findViewById(R.id.recyclerView);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawerMainActivity);
-
-        //Setup Titles and Icons of Navigation Drawer
-        navTitles = getResources().getStringArray(R.array.navDrawerItems);
-        navIcons = getResources().obtainTypedArray(R.array.navDrawerIcons);
-
-
-        /**
-         *Here , pass the titles and icons array to the adapter .
-         *Additionally , pass the context of 'this' activity .
-         *So that , later we can use the fragmentManager of this activity to add/replace fragments.
-         */
-
-        recyclerViewAdapter = new RecyclerViewAdapter(navTitles,navIcons,this);
-        recyclerView.setAdapter(recyclerViewAdapter);
-
-        /**
-         *It is must to set a Layout Manager For Recycler View
-         *As per docs ,
-         *RecyclerView allows client code to provide custom layout arrangements for child views.
-         *These arrangements are controlled by the RecyclerView.LayoutManager.
-         *A LayoutManager must be provided for RecyclerView to function.
-         */
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        //Finally setup ActionBarDrawerToggle
-        setupDrawerToggle();
-
-
-        //Add the Very First i.e Squad Fragment to the Container
-        Fragment FirstFragment = new FirstFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.containerView, FirstFragment, null);
-        fragmentTransaction.commit();
-
-
-
-
-    }
-
-    void setupToolbar(){
-        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        // Initializing Toolbar and setting it as the actionbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //Initializing NavigationView
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()){
+
+
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.inbox:
+                        Toast.makeText(getApplicationContext(),"Inbox Selected",Toast.LENGTH_SHORT).show();
+                        Fragment fragment = new FirstFragment();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.frame,fragment);
+                        fragmentTransaction.commit();
+                        return true;
+
+                    // For rest of the options we just show a toast on click
+
+                    case R.id.starred:
+                        Toast.makeText(getApplicationContext(),"Stared Selected",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.sent_mail:
+                        Toast.makeText(getApplicationContext(),"Send Selected",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.drafts:
+                        Toast.makeText(getApplicationContext(),"Drafts Selected",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.allmail:
+                        Toast.makeText(getApplicationContext(),"All Mail Selected",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.trash:
+                        Toast.makeText(getApplicationContext(),"Trash Selected",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"Inbox Selected",Toast.LENGTH_SHORT).show();
+                        fragment = new SixthFragment();
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.frame,fragment);
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.spam:
+                        Toast.makeText(getApplicationContext(),"Spam Selected",Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(),"Somethings Wrong",Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+            }
+        });
+
+        View headerView = navigationView.inflateHeaderView(R.layout.header);
+        TextView Name = (TextView) headerView.findViewById(R.id.textViewName);
+        TextView Email = (TextView) headerView.findViewById(R.id.textViewEmail);
+        Name.setText(LoginActivity.User_name);
+        Email.setText(LoginActivity.User_email);
+        profilePhoto = (NetworkImageView)headerView.findViewById(R.id.profileImage);
+        imageLoader = CustomVolleyRequest.getInstance(this)
+                .getImageLoader();
+        String noImageURL = "http://static.sgv2.com/img/185242/no-image.png";
+        if(LoginActivity.User_photourl!=null)
+        {
+
+            //Initializing image loader
+
+
+            imageLoader.get(LoginActivity.User_photourl,
+                    ImageLoader.getImageListener(profilePhoto,
+                            R.mipmap.ic_launcher,
+                            R.mipmap.ic_launcher));
+
+            //Loading image
+            profilePhoto.setImageUrl(LoginActivity.User_photourl, imageLoader);
+        }
+        else
+        {
+            imageLoader.get(noImageURL,
+                    ImageLoader.getImageListener(profilePhoto,
+                            R.mipmap.ic_launcher,
+                            R.mipmap.ic_launcher));
+            profilePhoto.setImageUrl(noImageURL, imageLoader);
+        }
+
+        // Initializing Drawer Layout and ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+
+
+
+
+
     }
 
-    void setupDrawerToggle(){
-        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
-        //This is necessary to change the icon of the Drawer Toggle upon state change.
-        drawerToggle.syncState();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
 }
