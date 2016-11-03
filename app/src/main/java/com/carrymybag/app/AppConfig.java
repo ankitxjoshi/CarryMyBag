fdd02ceb5ac02673cbbcb7dda4fd0f04ea87960a
kumaranshu72 <kumaranshu72@gmail.com>
2016-11-03 10:46:38 +0530

    Final Push by Lucifer ;) All is Well!!

--- app/src/main/java/com/carrymybag/app/AppConfig.java
+++ app/src/main/java/com/carrymybag/app/AppConfig.java
@@ -45,20 +45,20 @@ public class AppConfig {
 */
 	/////////////////////////////////////////////////////////////////////////////////////////
 
-	public static String URL_CONTACTUS = "http://hladnitkkr.in//contactus.php";
-	public static String URL_LOGIN = "http://hladnitkkr.in//login.php";
+	public static String URL_CONTACTUS = "http://carrymybag.000webhostapp.com//contactus.php";
+	public static String URL_LOGIN = "http://carrymybag.000webhostapp.com//login.php";
 	// Server user register url
-	public static String URL_REGISTER = "http://hladnitkkr.in//register.php";
+	public static String URL_REGISTER = "http://carrymybag.000webhostapp.com//register.php";
 //	// Server user retrieve city url
-	public static String URL_CityList = "http://hladnitkkr.in//retreivecity.php";
-	public static String URL_BagList = "http://hladnitkkr.in//retreivebaglist.php";
-	public static String URL_FORGOTPASS = "http://hladnitkkr.in//forgotpass.php";
-	public static String URL_StoreLuggage = "http://hladnitkkr.in//luggage.php";
-	public static String URL_StoreOrder = "http://hladnitkkr.in//order_details.php";
-	public static String URL_StoreUser = "http://hladnitkkr.in//user_detail.php";
-	public static String URL_GetPrice = "http://hladnitkkr.in//getprice.php";
-	public static String URL_CHANGEPASS = "http://hladnitkkr.in//changepass.php";
-	public static String URL_PRICEFACTOR = "http://hladnitkkr.in//pricefactor.php";
+	public static String URL_CityList = "http://carrymybag.000webhostapp.com//retreivecity.php";
+	public static String URL_BagList = "http://carrymybag.000webhostapp.com//retreivebaglist.php";
+	public static String URL_FORGOTPASS = "http://carrymybag.000webhostapp.com//forgotpass.php";
+	public static String URL_StoreLuggage = "http://carrymybag.000webhostapp.com//luggage.php";
+	public static String URL_StoreOrder = "http://carrymybag.000webhostapp.com//order_details.php";
+	public static String URL_StoreUser = "http://carrymybag.000webhostapp.com//user_detail.php";
+	public static String URL_GetPrice = "http://carrymybag.000webhostapp.com//getprice.php";
+	public static String URL_CHANGEPASS = "http://carrymybag.000webhostapp.com//changepass.php";
+	public static String URL_PRICEFACTOR = "http://carrymybag.000webhostapp.com//pricefactor.php";
 ////
 
 }
